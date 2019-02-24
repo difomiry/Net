f0098b83953ec5d6eb361df74ae3320ccad59246
Dmitry Fomin <difomiry@gmail.com>
2019-02-24 11:45:29 +0000

    Refactored `Request`, `Requestable`

--- Sources/Requestable.swift
+++ Sources/Requestable.swift
@@ -7,7 +7,7 @@ public typealias Parameters = [String: Any]
 public typealias Headers = [String: Any]
 
 /// Describes the common request attributes.
-public protocol Requestable {
+public protocol Requestable: URLConvertible, URLRequestConvertible {
 
   /// The base `URL`.
   var baseURL: URLConvertible { get }
@@ -62,6 +62,79 @@ public extension Requestable {
   /// The empty request credentials.
   public var credentials: Credentials? {
     return nil
+  }
+
+}
+
+public extension URLConvertible where Self: Requestable {
+
+  /// Builds the `URL`.
+  ///
+  /// - Throws: Any error thrown while building the `URL`.
+  /// - Returns: The builded `URL`.
+  public func toURL() throws -> URL {
+
+    var url = try baseURL.toURL()
+
+    if let path = path {
+      url.appendPathComponent(path)
+    }
+
+    var urlComponents = try URLComponents(url: url) ??? Error.invalidPath(path: path ?? "")
+
+    var parameters: Parameters = self.parameters ?? [:]
+
+    if let credentials = credentials, case let .apiKey(key, value) = credentials {
+      parameters[key] = value
+    }
+
+    urlComponents.queryItems = try parameters.toQueryItems()
+
+    return try urlComponents.toURL()
+  }
+
+}
+
+public extension URLRequestConvertible where Self: Requestable {
+
+  /// Builds the `URLRequest`.
+  ///
+  /// - Throws: Any error thrown while building the `URLRequest`.
+  /// - Returns: The builded `URLRequest`.
+  public func toURLRequest() throws -> URLRequest {
+
+    var urlRequest = URLRequest(url: try toURL())
+
+    urlRequest.httpBody = body
+
+    headers?.forEach { (key, value) in urlRequest.addValue("\(value)", forHTTPHeaderField: key) }
+
+    func addAuthorizationHeader(value: String) {
+      urlRequest.addValue(value, forHTTPHeaderField: "Authorization")
+    }
+
+    if let credentials = credentials {
+      switch credentials {
+      case .apiKey:
+        break
+      case let .basicAuthentication(username, password):
+        addAuthorizationHeader(value: "Basic \((username + ":" + password).data(using: .utf8)!.base64EncodedString())")
+      case let .bearerAuthentication(token):
+        addAuthorizationHeader(value: "Bearer \(token)")
+      }
+    }
+
+    urlRequest.httpMethod = method.rawValue
+
+    return urlRequest
+  }
+
+}
+
+fileprivate extension URLComponents {
+
+  init?(url: URL) {
+    self.init(url: url, resolvingAgainstBaseURL: true)
   }
 
 }
